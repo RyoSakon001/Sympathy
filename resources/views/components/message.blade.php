a5c19881f54cc3814ed75ad01f94e97cb2943ce1
Ryo-Sakon <ryo.development0320@gmail.com>
2022-03-20 09:19:00 +0900

    Componentを用いて投稿OK文にTailwindCSSを適用

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,6 @@
+@if (isset($message))
+<div class="border px-4 py-3 rounded relative bg-green-100 border-green-400 text-green-700">
+    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+    {{$message}}
+</div>
+@endif
